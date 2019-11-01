4d3294e86cda5de1ed9a4d6582c0e3da5377e5ef
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-01 11:07:10 -0500

    creating first solution for the chunk array challenge

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// my first solutiom
+function chunk(array, size) {
+    const numberOfChunks = Math.ceil(array.length / size);
+    const chunkedArray = [];
+    let currentArrayIndex = 0;
+
+    for (let index = 0; index < numberOfChunks; index++) {
+        const chunk = [];
+
+        for (let chunkIndex = 0; chunkIndex < size && currentArrayIndex < array.length; chunkIndex++) {
+            const currentArrayElement = array[currentArrayIndex];
+
+            chunk[chunkIndex] = currentArrayElement;
+            currentArrayIndex++;
+        }
+
+        chunkedArray[index] = chunk;
+    }
+
+    return chunkedArray;
+};
 
 module.exports = chunk;
