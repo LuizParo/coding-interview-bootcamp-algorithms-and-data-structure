3e5cab123ee9a41868ab94b80b6e1581bfb4dbff
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-10-31 22:32:41 -0500

    creating first solution to find is a word is palindrome or not

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// my first solution
+function palindrome(str) {
+    let isPalindrome = true;
+
+    for (let index = 0, reverseIndex = str.length - 1; index < Math.ceil(str.length / 2); index++, reverseIndex--) {
+        isPalindrome = isPalindrome && (str.charAt(index) == str.charAt(reverseIndex));
+    }
+
+    return isPalindrome;
+};
 
 module.exports = palindrome;
