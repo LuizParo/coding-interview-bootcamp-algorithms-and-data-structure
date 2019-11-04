f7d56e58c8b4e44622ab0e10b3e8657b5fdf11a8
Luiz Guilherme Paro <luiz.paro@skipthedishes.ca>
2019-11-04 16:41:10 -0600

    adding another solution for the fibonacci challenge

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -29,17 +29,26 @@
 // };
 
 // solution #1
+// function fib(n) {
+//     const result = [0, 1];
+
+//     for (let i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+
+//         result.push(a + b);
+//     }
+
+//     return result[n];
+// };
+
+// solution #2
 function fib(n) {
-    const result = [0, 1];
-
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
-
-        result.push(a + b);
+    if (n < 2) {
+        return n;
     }
 
-    return result[n];
+    return fib(n - 1) + fib(n - 2);
 };
 
 module.exports = fib;
