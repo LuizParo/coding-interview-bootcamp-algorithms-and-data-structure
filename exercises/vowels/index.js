7e34225a744c1a8a481f43d28ef307793413b538
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-03 23:20:22 -0600

    creating another solution for the vowel counter challenge

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,19 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function _isVowel(char) {
-    return char === 'a'
-        || char == 'e'
-        || char == 'i'
-        || char == 'o'
-        || char == 'u';
-};
+// function _isVowel(char) {
+//     return char === 'a'
+//         || char == 'e'
+//         || char == 'i'
+//         || char == 'o'
+//         || char == 'u';
+// };
 
+// my first solution
+// function vowels(str) {
+//     return str.toLowerCase()
+//               .split('')
+//               .filter(_isVowel)
+//               .length;
+// };
+
+// solution #1
 function vowels(str) {
-    return str.toLowerCase()
-              .split('')
-              .filter(_isVowel)
-              .length;
+    let count = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
 };
 
 module.exports = vowels;
