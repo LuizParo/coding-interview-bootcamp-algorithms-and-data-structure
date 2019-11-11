ab98bc69e597d267741518f6a03892967861fdfa
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-10 22:41:07 -0600

    creating first solution for the from last linked list node challenge

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,21 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+// my first solution
+function fromLast(list, n) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    while(fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+};
 
 module.exports = fromLast;
