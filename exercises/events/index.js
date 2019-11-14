affc9b01452292bd6305ddbdacb4c31dfeeb370a
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-13 19:21:26 -0600

    creating first solution for the events challenge

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,41 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+    // my first solution
+    constructor() {
+        this.eventMap = {};
+    }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+    // Register an event handler
+    on(eventName, callback) {
+        const events = this.eventMap[eventName];
+
+        if (!events) {
+            this.eventMap[eventName] = [callback];
+            return;
+        }
+
+        events.push(callback);
+    }
+
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        const events = this.eventMap[eventName];
+
+        if (!events) {
+            return;
+        }
+
+        events.forEach(callback => callback());
+    }
+
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        delete this.eventMap[eventName];
+    }
 }
 
 module.exports = Events;
