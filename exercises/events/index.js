61366b7e03373021afc45bc883646caaaa1e041e
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-13 19:26:57 -0600

    creating another solution for the events challenge

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -6,38 +6,68 @@
 class Events {
 
     // my first solution
+    // constructor() {
+    //     this.eventMap = {};
+    // }
+
+    // // Register an event handler
+    // on(eventName, callback) {
+    //     const events = this.eventMap[eventName];
+
+    //     if (!events) {
+    //         this.eventMap[eventName] = [callback];
+    //         return;
+    //     }
+
+    //     events.push(callback);
+    // }
+
+    // // Trigger all callbacks associated
+    // // with a given eventName
+    // trigger(eventName) {
+    //     const events = this.eventMap[eventName];
+
+    //     if (!events) {
+    //         return;
+    //     }
+
+    //     events.forEach(callback => callback());
+    // }
+
+    // // Remove all event handlers associated
+    // // with the given eventName
+    // off(eventName) {
+    //     delete this.eventMap[eventName];
+    // }
+
+    // solution #1
     constructor() {
-        this.eventMap = {};
+        this.events = {};
     }
 
     // Register an event handler
     on(eventName, callback) {
-        const events = this.eventMap[eventName];
-
-        if (!events) {
-            this.eventMap[eventName] = [callback];
-            return;
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback);
+        } else {
+            this.events[eventName] = [callback];
         }
-
-        events.push(callback);
     }
 
     // Trigger all callbacks associated
     // with a given eventName
     trigger(eventName) {
-        const events = this.eventMap[eventName];
-
-        if (!events) {
-            return;
+        if (this.events[eventName]) {
+            for (let callback of this.events[eventName]) {
+                callback();
+            }
         }
-
-        events.forEach(callback => callback());
     }
 
     // Remove all event handlers associated
     // with the given eventName
     off(eventName) {
-        delete this.eventMap[eventName];
+        delete this.events[eventName];
     }
 }
 
