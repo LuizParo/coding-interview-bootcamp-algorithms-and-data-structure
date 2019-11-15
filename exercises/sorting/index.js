b28007db0c8b32a4695cd70dfb9272d8d0630c54
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-11-14 20:47:36 -0600

    creating another solution for the bubble sorst challenge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,15 +2,30 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 // my first implementation
+// function bubbleSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - i - 1; j++) {
+//             const current = arr[j];
+//             const next = arr[j + 1];
+
+//             if (current > next) {
+//                 arr[j + 1] = current;
+//                 arr[j] = next
+//             }
+//         }
+//     }
+
+//     return arr;
+// }
+
+// solution #1
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
-            const current = arr[j];
-            const next = arr[j + 1];
-
-            if (current > next) {
-                arr[j + 1] = current;
-                arr[j] = next
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser
             }
         }
     }
